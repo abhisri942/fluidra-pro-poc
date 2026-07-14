443a201b704e830fe52ec6a0f55b4054eff9db4c
abhisri942 <abhisheksrivastava942@gmail.com>
2026-07-14 21:57:28 +0530

    chore: cleanup old models, update source to POOLPRO_INBOUND_EVENTS, add .gitignore
    - Removed old dimensions, facts, intermediate, marts, reporting models
    - Removed old snapshot and tests referencing deleted models
    - Updated source to ANALYTICS_DB_PROD.RAW_FLUIDRA_PRO.POOLPRO_INBOUND_EVENTS
    - Added .gitignore for dbt artifacts (target/, logs/, dbt_packages/, profiles.yml)

--- dbt/tests/test_kpi_dealer_adoption.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Custom Test: Validate dealer adoption KPI business rules
-*/
-WITH mart AS (SELECT * FROM {{ ref('mart_dealer_adoption') }})
-SELECT * FROM mart
-WHERE active_dealers > total_dealers
-    OR active_dealer_rate < 0 OR active_dealer_rate > 1
-    OR enrolled_dealers > total_dealers
